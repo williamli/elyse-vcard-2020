1c714713488cad930c74a705cd1f846f3afc3730
William Li <williamli@bbi.io>
2020-02-15 02:26:22 +0800

    reload page when screen rotates

--- src/js/card.js
+++ src/js/card.js
@@ -1,3 +1,13 @@
 function toggleCard () {
   $('.card-container').toggleClass('opened');
 }
+
+
+window.onorientationchange = function() { 
+  var orientation = window.orientation; 
+    switch(orientation) { 
+        case 0:
+        case 90:
+        case -90: window.location.reload(); 
+        break; } 
+};
